65ea91af2fdd9bbf38303ccb12234f432b2e39fb
Pavan Jadhaw <pavanjadhaw96@gmail.com>
2018-12-25 12:41:30 +0530

    [initial] prerelease

---
+++ cli.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+'use strict';
+const { createReadStream } = require('fs');
+const got = require('got');
+const ora = require('ora');
+const meow = require('meow');
+const FormData = require('form-data');
+
+const cli = meow(
+  `
+	Usage
+    $ up <file> …
+
+  Examples
+	  $ up strangers.mp3
+	  $ up sigrid.png
+`,
+  {
+    flags: {
+      init: {
+        type: 'boolean',
+        alias: 'i',
+      },
+      help: {
+        alias: 'h',
+      },
+      version: {
+        alias: 'v',
+      },
+    },
+  }
+);
+
+const file = cli.input[0];
+
+const domain = process.env.SPRK_SERVER_URL;
+const apiKey = process.env.SPRK_API_KEY;
+
+if (!domain) {
+  console.error(
+    'Server URL not available, Please set SPRK_SERVER_URL env variable.'
+  );
+  process.exit(1);
+}
+
+if (!apiKey) {
+  console.error('Server API key not available, set SPRK_API_KEY env variable.');
+  process.exit(1);
+}
+
+(async () => {
+  if (file) {
+    const form = new FormData();
+    form.append('file', createReadStream(file));
+
+    const spinner = ora(`Uploading ${file}…`).start();
+
+    got(domain, {
+      method: 'POST',
+      headers: {
+        'Content-Type': `multipart/form-data; boundary=${form._boundary}`,
+        'api-key': 'YHGAnr8kJUmPhjS30ilmd9mCTLQ6pAg0',
+      },
+      body: form,
+    })
+      .then(response => {
+        if (response.statusCode === 200) {
+          spinner.succeed(`File uploaded... ${response.body}`);
+        }
+      })
+      .catch(error => {
+        if (error.statusCode === 400)
+          spinner.fail('Bad request, often due to missing parameter.');
+        else if (error.statusCode === 401)
+          spinner.fail('No valid API key provided');
+        else if (error.statusCode === 404)
+          spinner.fail("The requested resource doesn't exists");
+        else spinner.fail('Unknown error', error.message);
+      });
+  } else {
+    console.error('Specify a file to upload...');
+    process.exit(1);
+  }
+})();
