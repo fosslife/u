b4ac6f954661a113da85aa99ec5a1540c6e3a51e
Sparkenstein <zetabytes.pp@gmail.com>
2019-01-08 22:30:47 +0530

    added check for env variables present

---
+++ core/base.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const die = require('./die');
+
+const allgood = (domain, apiKey) => {
+    if (!domain) {
+        die('Server URL not available, Please set SPRK_SERVER_URL env variable.')
+    }
+    if (!apiKey) {
+        die('Server API key not available, set SPRK_API_KEY env variable.')
+    }
+    return true;
+}
+
+module.exports = allgood;
