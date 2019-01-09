26a2ec0294643896d13fc4cbacb2760f558749f1
Sparkenstein <zetabytes.pp@gmail.com>
2019-01-10 00:06:33 +0530

    refactored headers for urls

--- core/headers.js
+++ core/headers.js
@@ -7,9 +7,11 @@
  * @param {length} l formdata boundry length
  */
 const headers = (k, l) => {
-    return {
+    return l && {
         'Content-Type': `multipart/form-data; boundary=${l}`,
         'api-key': k,
+    } || {
+        'api-key': k
     }
 }
 
